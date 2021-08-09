235454d62ce2639c788647a1aad229eb42a4e1bd
simonLuis <42242167+simonLuis@users.noreply.github.com>
2021-08-08 23:03:15 -0300

    Aprendendo a fazer um qrcode simples

---
+++ QrCode/qrcode.py
@@ -0,0 +1,15 @@
+import qrcode
+
+url = 'https://www.instagram.com/gustavo_simon5/'
+
+qr = qrcode.QRcode(
+    version=1,
+    box_size=4,
+    border=5
+)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+img.show()
